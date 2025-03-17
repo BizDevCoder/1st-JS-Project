c22aeb194f7b58c0cef2787c3065d359fc73c57a
Rohit Kumar <rohit20021004@gmail.com>
2025-03-18 02:44:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById("searchBtn");
+const input = document.getElementById("city-input");
+
+const cityName = document.getElementById("city-name")
+const cityTime = document.getElementById("city-time")
+const cityTemp = document.getElementById("city-temp")
+
+async function getData(cityname){
+    const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=c2f88823e8654f3197d81522251703&q=${cityname}&aqi=yes`);
+    return await promise.json()
+}
+
+btn.addEventListener("click", async () => {
+    const value = input.value;
+    const result = await getData(value);
+    cityName.innerText =   `${result.location.name}, ${result.location.region} - ${result.location.country}`
+    cityTime.innerText =   result.location.localtime;
+    cityTemp.innerText =   result.current.temp_c;
+});
